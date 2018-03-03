6b6b7bf057ea92aaf6199ca200b6adfd107724cd
Daniel Burke <burkdan@umich.edu>
2018-03-02 19:32:48 -0500

    working for multiple events

--- google_calendar_event.py
+++ google_calendar_event.py
@@ -1,5 +1,5 @@
 """Modification of the url-text default script in OBS to get events from the Google Calendar API"""
-# Created by burkdan@umich.edu
+# Coded by Daniel Burke
 
 from __future__ import print_function
 import httplib2
@@ -18,15 +18,14 @@ import datetime
 
 cal_url     = ""
 interval    = 30
-source_name = ""
-# source_names = {}
+max_events  = 3
+source_names = [""]*max_events
 
 # ------------------------------------------------------------
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-# CLIENT_SECRET_FILE = 'C:/users/Daniel/Desktop/School/Design/DS_projector/client_secret.json'
 CLIENT_SECRET_FILE = ''
 APPLICATION_NAME = 'Google Calendar OBS event script'
 
@@ -56,14 +55,14 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+# ------------------------------------------------------------
+
 def update_text():
     global cal_url
     global interval
-    global source_name
-    # global source_names
+    global source_names
     global CLIENT_SECRET_FILE
-
-    # if source is not None:
+    global max_events
         
     # Gets stored credentials (taken from Calendar API quickstart)
     credentials = get_credentials()
@@ -73,31 +72,41 @@ def update_text():
     # Time objects using datetime
     dt_now = datetime.datetime.utcnow()
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    # Gets events currently happending by setting bounds to events happening within a second of current datetime, max ten events 
-    events = service.events().list(calendarId=cal_url, timeMin=now, timeMax=(dt_now+datetime.timedelta(0,1)).isoformat() +'Z', maxResults=10, singleEvents=True,
-                            orderBy='startTime').execute()
+
+    # Gets events currently happending by setting bounds to events happening within a second of current datetime
+    events = service.events().list(calendarId=cal_url, timeMin=now, timeMax=(dt_now+datetime.timedelta(0,1)).isoformat() +'Z',
+                                   maxResults=max_events, singleEvents=True, orderBy='startTime').execute()
     
+    # Logs the events to console
     for event in events['items']:
         print(event['summary'])
 
+    # Updates the text for each event
+    count = 0
+    for event in events['items']:
+        if(count >= max_events):
+            break
+        text = event['summary']
+        settings = obs.obs_data_create()
+        obs.obs_data_set_string(settings, "text", text)
+        source = obs.obs_get_source_by_name(source_names[count])
+        obs.obs_source_update(source, settings)
+        obs.obs_data_release(settings)
+        obs.obs_source_release(source)
+        count += 1
 
-    text = events['items'][0]['summary']
-
+    # Sets any specified sources to blank if here is no event 
+    text = ""
     settings = obs.obs_data_create()
     obs.obs_data_set_string(settings, "text", text)
-    source = obs.obs_get_source_by_name(source_name)
-    obs.obs_source_update(source, settings)
-    obs.obs_data_release(settings)
-    obs.obs_source_release(source)
+    for x in range(count, max_events):
+        source = obs.obs_get_source_by_name(source_names[x])
+        obs.obs_source_update(source, settings)
+        obs.obs_data_release(settings)
+        obs.obs_source_release(source)
 
-    # for event in events['items']:
-    #     text = event['summary']
-    #     settings = obs.obs_data_create()
-    #     obs.obs_data_set_string(settings, "text", text)
-    #     source = obs.obs_get_source_by_name(source_names[text])
-    #     obs.obs_source_update(source, settings)
-    #     obs.obs_data_release(settings)
-    #     obs.obs_source_release(source)
+
+# ------------------------------------------------------------
 
 def refresh_pressed(props, prop):
     update_text()
@@ -107,28 +116,36 @@ def refresh_pressed(props, prop):
 def script_description():
     return "Upates text based on a Google Calendar event"
 
+# ------------------------------------------------------------
+
 def script_update(settings):
     global cal_url
     global interval
-    global source_name
-    # global source_names
+    global source_names
     global CLIENT_SECRET_FILE
+    global max_events
 
     cal_url                = obs.obs_data_get_string(settings, "calendar_url")
     CLIENT_SECRET_FILE     = obs.obs_data_get_string(settings, "client_secret_file")
     interval               = obs.obs_data_get_int(settings, "interval")
-    source_name            = obs.obs_data_get_string(settings, "source")
-    # for source_name in source_names:
-    #      obs.obs_data_get_string(settings, "source")
-    
+    max_events             = obs.obs_data_get_int(settings, "max_events")
+
+    source_names = [None]*max_events
+    for x in range(0, max_events):
+       source_names[x] = obs.obs_data_get_string(settings, "source_{}".format(x))
 
     obs.timer_remove(update_text)
 
     if cal_url != "": #and source_name != "":
         obs.timer_add(update_text, interval * 1000)
 
+# ------------------------------------------------------------
+
 def script_defaults(settings):
     obs.obs_data_set_default_int(settings, "interval", 30)
+    obs.obs_data_set_default_int(settings, "max_events", 3)
+
+# ------------------------------------------------------------
 
 def script_properties():
     props = obs.obs_properties_create()
@@ -136,17 +153,21 @@ def script_properties():
     obs.obs_properties_add_text(props, "calendar_url", "Calendar URL", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_path(props, "client_secret_file", "Client Secret File", obs.OBS_PATH_FILE,'*.json', "")
     obs.obs_properties_add_int(props, "interval", "Update Interval (seconds)", 5, 3600, 1)
+    obs.obs_properties_add_int(props, "max_events", "Max Number of Events", 1, 15, 1)
 
-    p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
-    sources = obs.obs_enum_sources()
-    if sources is not None:
-        for source in sources:
-            source_id = obs.obs_source_get_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
-                name = obs.obs_source_get_name(source)
-                obs.obs_property_list_add_string(p, name, name)
+    for x in range(0,max_events):
+        p = obs.obs_properties_add_list(props, "source_{}".format(x), "Text Source {}".format(x + 1), 
+                                        obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 
-        obs.source_list_release(sources)
+        sources = obs.obs_enum_sources()
+        if sources is not None:
+            for source in sources:
+                source_id = obs.obs_source_get_id(source)
+                if source_id == "text_gdiplus" or source_id == "text_ft2_source":
+                    name = obs.obs_source_get_name(source)
+                    obs.obs_property_list_add_string(p, name, name)
+
+            obs.source_list_release(sources)
 
     obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
     return props
